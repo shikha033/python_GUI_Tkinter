f4708b659cd9767e8d188b018536963974213070
shikha033 <shikhasingh242494@gmail.com>
2024-12-28 22:04:04 +0530

    ImageTk

--- images_tk.py
+++ images_tk.py
@@ -1,12 +1,19 @@
 from tkinter import *
 root= Tk()  # small k
+from PIL import Image, ImageTk # before this install pillow
 
-root.geometry("444x244")
+root.geometry("1920x1080")
 
 # images  related widgets
-photo=PhotoImage(file="img.png")
+'''photo=PhotoImage(file="img.png")
 label1=Label(image=photo)
-label1.pack()
+label1.pack()'''
+
+# for jpgimages--
+image2=Image.open("img2.jpeg")
+photo2=ImageTk.PhotoImage(image2)
+label2=Label(image=photo2)
+label2.pack()
 
 
 
