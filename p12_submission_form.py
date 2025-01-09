e6fa26d1db9e3a2158a2c5752715e71eeb6d60a3
shikha033 <shikhasingh242494@gmail.com>
2025-01-09 21:20:15 +0530

    Update p12_submission_form.py

--- p12_submission_form.py
+++ p12_submission_form.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 root = tk.Tk()
+root.configure(bg="black")
+root.geometry("400x200")
 root.title("Submission Form System")
 def submit_form(): 
 # Retrieve input values 
@@ -13,18 +15,18 @@ def submit_form():
     age_entry.delete(0, tk.END)
 
 
-tk.Label(root, text="Name:").grid(row=0, column=0, padx=10, pady=5, sticky="e") 
+tk.Label(root, text="Name:").grid(row=0, column=5, padx=10, pady=5, sticky="e") 
 name_entry = tk.Entry(root) 
-name_entry.grid(row=0, column=1, padx=10, pady=5) 
-tk.Label(root, text="Email:").grid(row=1, column=0, padx=10, pady=5, sticky="e") 
+name_entry.grid(row=0, column=6, padx=10, pady=5) 
+tk.Label(root, text="Email:").grid(row=1, column=5, padx=10, pady=5, sticky="e") 
 email_entry = tk.Entry(root) 
-email_entry.grid(row=1, column=1, padx=10, pady=5) 
-tk.Label(root, text="Age:").grid(row=2, column=0, padx=10, pady=5, sticky="e") 
+email_entry.grid(row=1, column=6, padx=10, pady=5) 
+tk.Label(root, text="Age:").grid(row=2, column=5, padx=10, pady=5, sticky="e") 
 age_entry = tk.Entry(root) 
-age_entry.grid(row=2, column=1, padx=10, pady=5) 
+age_entry.grid(row=2, column=6, padx=10, pady=5) 
 # Add Submit Button 
-submit_button = tk.Button(root, text="Submit", command=submit_form) 
-submit_button.grid(row=3, column=0, columnspan=2, pady=10)
+submit_button = tk.Button(root, text="Submit", command=submit_form,bg='red' )
+submit_button.grid(row=3, column=5, columnspan=2, pady=10)
 
 
 
