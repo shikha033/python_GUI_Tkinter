549cc5f977ff7f0ba14191b200307ca65ae676b4
shikha033 <shikhasingh242494@gmail.com>
2025-02-06 22:22:01 +0530

    Update p16_sliders.py

--- p16_sliders.py
+++ p16_sliders.py
@@ -9,6 +9,11 @@ root.title("Slider tutorial")
 def getdollar():
     print(f"We have credited {myslider2.get()} dollars to your bank account")
     tmsg.showinfo("Amount Credited!", f"We have credited {myslider2.get()} dollars to your bank account")
+
 # myslider = Scale(root, from_=0, to=100)
 # myslider.pack()
 Label(root, text="How many dollars do you want?").pack()
+myslider2 = Scale(root, from_=0, to=100, orient=HORIZONTAL, tickinterval=50)
+# myslider2.set(34)
+myslider2.pack()
+
