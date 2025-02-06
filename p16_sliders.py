64bff673d3bbde39c0edb9b948baa73b4843a2ba
shikha033 <shikhasingh242494@gmail.com>
2025-02-06 22:25:45 +0530

    Update p16_sliders.py

--- p16_sliders.py
+++ p16_sliders.py
@@ -17,3 +17,7 @@ myslider2 = Scale(root, from_=0, to=100, orient=HORIZONTAL, tickinterval=50)
 # myslider2.set(34)
 myslider2.pack()
 
+
+Button(root, text="Get dollars!", pady=10, command=getdollar).pack()
+
+root.mainloop()
