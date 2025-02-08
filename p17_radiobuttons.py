b3c5da67bc92e0d4f7f35257b2ca8da50ea10d09
shikha033 <shikhasingh242494@gmail.com>
2025-02-08 23:34:32 +0530

    Update p17_radiobuttons.py

--- p17_radiobuttons.py
+++ p17_radiobuttons.py
@@ -3,3 +3,12 @@ import tkinter.messagebox as tmsg
 root = Tk()
 root.geometry("455x233")
 root.title("Radiobutton tutorial")
+
+def order():
+
+    tmsg.showinfo("Order Received!", f"We have received your order for {var.get()}. Thanks for ordering")
+
+# var = IntVar()
+var = StringVar()
+var.set("Radio")
+# var.set(1)
