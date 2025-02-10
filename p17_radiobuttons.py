c5fae2f83eee4c7f297ffd8d8c5b62cc43c86ca7
shikha033 <shikhasingh242494@gmail.com>
2025-02-10 23:06:00 +0530

    Update p17_radiobuttons.py

--- p17_radiobuttons.py
+++ p17_radiobuttons.py
@@ -17,6 +17,8 @@ Label(root, text = "What would you like to have sir?",font="lucida 19 bold",
       justify=LEFT, padx=14).pack()
 radio = Radiobutton(root, text="Dosa", padx=14, variable=var, value="Dosa").pack(anchor="w")
 radio = Radiobutton(root, text="Idly", padx=14, variable=var, value="Idly").pack(anchor="w")
+radio = Radiobutton(root, text="Paratha", padx=14, variable=var, value="Paratha").pack(anchor="w")
+radio = Radiobutton(root, text="Samosa", padx=14, variable=var, value="Samosa").pack(anchor="w")
 
 Button(text="Order Now", command=order).pack()
 root.mainloop()
